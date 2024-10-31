a8c990cce369e1fd498d1fd17950eb14bf3818db
Abdulkadir Burak AFŞAR <burakafsar4066@gmail.com>
2024-10-31 20:42:31 +0300

    Add files via upload

---
+++ Hipotenus.py
@@ -0,0 +1,8 @@
+print("Dik Üçgenin Hipotenüsünü Bulma")
+
+a=float(input("Birinci dik kanarı girin:\n"))
+b=float(input("İkinci dik kanarı  girin:\n"))
+
+c= (a**2+b**2)**0.5
+
+print("Hipotenüs {}'dir".format(c))
